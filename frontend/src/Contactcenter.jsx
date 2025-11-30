0c1c1d499c84c1a4d17bb38fca34b5aa90294ba5
Anushka Mathane <anumathane1@gmail.com>
2025-11-30 16:59:54 +0530

    Fix case-sensitive import issue

--- frontend/src/Contactcenter.jsx
+++ frontend/src/Contactcenter.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "./ContactCenter.css";
+import "./Contactcenter.css";
 import avatarImg from "/images/img.png";
 import { useTeam } from "./Teamcontext";
 
